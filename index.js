8cc4cac12ba4c0563f89a9a124bd80d0319aa5da
Garima Bisht <74089873+GariBisht@users.noreply.github.com>
2025-03-15 00:08:17 +0530

    changes

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ const userSchema = Joi.object({
     password: Joi.string().min(6).required(),
 });
 
+const userSchemas = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().pattern(new RegExp("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$"))
+        .required()
+        .messages({
+            "string.pattern.base": "Password must contain at least one letter and one number.",
+        }),
+    age: Joi.number().integer().min(18).required(),
+});
+
+
+
+
 // Route to handle user registration
 app.post("/register", (req, res) => {
     const { error } = userSchema.validate(req.body);
